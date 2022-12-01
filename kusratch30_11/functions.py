895fb656aa9e1a2ba629e97e063caa50c62e8f55
Oleg Diachenko <116761572+dichenk@users.noreply.github.com>
2022-12-01 13:28:10 +0300

    Realized part of cash task.

--- kusratch30_11/functions.py
+++ kusratch30_11/functions.py
@@ -8,9 +8,20 @@ group_by_name = False
 order = 'AAL'
 filename='dump.csv'
 
+date_sec = "2017-08-08"
+name_sec="PCLN"
+filename_sec='dump_sec.csv'
+
+
 
 def select_sorted(sort_columns_f = sort_columns, limit_f = limit, group_by_name_f = group_by_name, order_f = order, filename_f = filename):
     dv = vaex.from_csv(file_path, convert=True)
     dvv = dv[dv.Name == order_f]
     dvv = dvv.sort(sort_columns_f, ascending=False)
     dvv[0:limit_f].export_csv(filename_f)
+
+def get_by_date(date = date_sec, name=name_sec, filename=filename_sec):
+    dv = vaex.from_csv(file_path, convert=True)
+    dvv = dv[dv.Name == name]
+    dvv = dvv[dv.date == date]
+    dvv.export_csv(filename)
--- kusratch30_11/main.py
+++ kusratch30_11/main.py
@@ -2,5 +2,6 @@ import functions
 
 def main():
     functions.select_sorted()
+    functions.get_by_date()
 
 main()
